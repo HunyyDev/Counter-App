b813d062ae4e74df1cf6cc43c185bd03c6c9dda9
phuochungus <21520252@gm.uit.edu.vn>
2023-01-03 15:20:04 +0700

    upload files

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnIncreaseCounter;
+    private TextView tvCounter;
+    private int Counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiate();
+    }
+
+    private void initiate() {
+        btnIncreaseCounter = findViewById(R.id.btnIncreaseCounter);
+        btnIncreaseCounter.setOnClickListener(this);
+        tvCounter = findViewById(R.id.tvCounter);
+    }
+
+    private void increaseCounter(){
+        ++Counter;
+    }
+
+    @Override
+    public void onClick(View v) {
+        increaseCounter();
+        String castedCounterToInt = Counter + "";
+        tvCounter.setText(castedCounterToInt);
     }
 }
